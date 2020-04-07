c3dbfb3039a7be1dd4303e4d4cab3a1319aa632d
nailham <63138897+nailham@users.noreply.github.com>
2020-04-08 00:51:15 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/latihan1/MainActivity.kt
+++ app/src/main/java/com/example/latihan1/MainActivity.kt
@@ -15,3 +15,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+/*
+Pengerjaan Tanggal : 05-04-2020
+Deskripsi pengerjaan : mengerjakan layout activity_biodata,kode,main,dan ok
+mengerjakan class yang terdiri dari biodata,kode,main_activity dan ok
+menambahkan values string dan drawable custom_button,comtum_ beserta coding
+Nim :10117138
+Nama :IlhamNurjaman
+Kelas :IF4/AKB
+*/
